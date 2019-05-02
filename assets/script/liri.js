5be3bea43de30fb6c1fec9072e076f199a0319a8
Russ <russ.dermody@gmail.com>
2019-05-01 17:57:13 -0700

    Began spot query. Current error: cant find dotenv

--- assets/script/liri.js
+++ assets/script/liri.js
@@ -2,13 +2,44 @@
 require('dotenv').config();
 //local keys file, GITignored
 var keys = require('./keys.js');
+// importing paxkages
+var spotify = require('node-spotify-api');
+var axios = require('axios');
+var moment = require('moment');
+var fs = require('fs');
 // BEGIN PSUDEOCODE
-
-// Need to require axios for APIs, moment for date/time formatting, and fs for reading/writing
 
 // Access spotify using the key, following syntax from instructions
 // BEGIN FIRST STEP PSUDEOCODE
+
 // Spotify API call
+// access spotify API using id+secret
+var spotify = new spotify(keys.spotify);
+
+var spotifySearch = function (songTitle) {
+  // error catch
+  if (songTitle === undefined) {
+    songTitle = 'NOTHING'
+  }
+  // send spotify the query
+  spotify.search(
+    {
+      type: 'track',
+      query: songTitle,
+    },
+    // errorcatch
+    function (err, data) {
+      if (err) {
+        console.log('ERROR : ' + err);
+        return;
+      }
+      // log result
+      var song = data;
+      console.log(song);
+
+    }
+  );
+}
 // write a function(songTitle) that queries spotify for a song, and returns
 // include an error catch 
 // grab whatever is coming back, and log it for all to see
