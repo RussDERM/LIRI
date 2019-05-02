b3be6e9c81ccaa3aacc0048368e8f0ecf8bcc42e
Russ <russ.dermody@gmail.com>
2019-05-01 17:04:04 -0700

    Psudeocoded the Spotify portion of the app.

--- assets/script/liri.js
+++ assets/script/liri.js
@@ -1,2 +1,15 @@
 //local config file, GITignored
 require('dotenv').config();
+//local keys file, GITignored
+var keys = require('./keys.js');
+// BEGIN PSUDEOCODE
+
+// Need to require axios for APIs, moment for date/time formatting, and fs for reading/writing
+
+// Access spotify using the key, following syntax from instructions
+// BEGIN FIRST STEP PSUDEOCODE
+// Spotify API call
+// write a function(songTitle) that queries spotify for a song, and returns
+// include an error catch 
+// grab whatever is coming back, and log it for all to see
+// peel the correct data out of there, and sent it to the console
